2e8a1672c2bc0b055cbdf5a3b6f17b28fbd04fe6
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2026-02-26 14:13:36 -0300

    fix(deploy): Fix deploy and source branches

--- tools/install-arduino.sh
+++ tools/install-arduino.sh
@@ -10,7 +10,7 @@ if [ ! -d "$AR_COMPS/arduino" ]; then
 	git clone $AR_REPO_URL "$AR_COMPS/arduino"
 fi
 
-if [ -z $AR_BRANCH ]; then
+if [ -z $AR_SOURCE_BRANCH ]; then
 	if [ -z $GITHUB_HEAD_REF ]; then
 		current_branch=`git branch --show-current`
 	else
@@ -18,31 +18,31 @@ if [ -z $AR_BRANCH ]; then
 	fi
 	echo "Current Branch: $current_branch"
 	if [[ "$current_branch" != "master" && `git_branch_exists "$AR_COMPS/arduino" "$current_branch"` == "1" ]]; then
-		export AR_BRANCH="$current_branch"
+		export AR_SOURCE_BRANCH="$current_branch"
 	else
-		if [ "$IDF_TAG" ]; then #tag was specified at build time
-			AR_BRANCH_NAME="idf-$IDF_TAG"
-		elif [ "$IDF_COMMIT" ]; then #commit was specified at build time
-			AR_BRANCH_NAME="idf-$IDF_COMMIT"
+		if [ "$IDF_COMMIT" ]; then #commit was specified at build time
+			AR_SOURCE_BRANCH_CANDIDATE="idf-$IDF_COMMIT"
+		elif [ "$IDF_TAG" ]; then #tag was specified at build time
+			AR_SOURCE_BRANCH_CANDIDATE="idf-$IDF_TAG"
 		else
-			AR_BRANCH_NAME="idf-$IDF_BRANCH"
+			AR_SOURCE_BRANCH_CANDIDATE="idf-$IDF_BRANCH"
 		fi
-		has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "$AR_BRANCH_NAME"`
+		has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "$AR_SOURCE_BRANCH_CANDIDATE"`
 		if [ "$has_ar_branch" == "1" ]; then
-			export AR_BRANCH="$AR_BRANCH_NAME"
+			export AR_SOURCE_BRANCH="$AR_SOURCE_BRANCH_CANDIDATE"
 		else
 			has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "$AR_PR_TARGET_BRANCH"`
 			if [ "$has_ar_branch" == "1" ]; then
-				export AR_BRANCH="$AR_PR_TARGET_BRANCH"
+				export AR_SOURCE_BRANCH="$AR_PR_TARGET_BRANCH"
 			fi
 		fi
 	fi
 fi
 
-if [ "$AR_BRANCH" ]; then
-	echo "AR_BRANCH='$AR_BRANCH'"
+if [ "$AR_SOURCE_BRANCH" ]; then
+	echo "AR_SOURCE_BRANCH='$AR_SOURCE_BRANCH'"
 	git -C "$AR_COMPS/arduino" fetch --all && \
-	git -C "$AR_COMPS/arduino" checkout "$AR_BRANCH" && \
+	git -C "$AR_COMPS/arduino" checkout -B "$AR_SOURCE_BRANCH" origin/"$AR_SOURCE_BRANCH" && \
 	git -C "$AR_COMPS/arduino" pull --ff-only
 fi
 if [ $? -ne 0 ]; then exit 1; fi
